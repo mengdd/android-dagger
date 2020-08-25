df21adcc3dc9119ab2761a715323f6324f0989b7
Dandan Meng <mengdandanno1@163.com>
2020-08-25 23:38:16 +0800

    step 12: subcomponent for login flow

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationComponent
 import dagger.BindsInstance
@@ -20,6 +21,7 @@ interface AppComponent {
 
     // Expose RegistrationComponent factory from the graph
     fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
 
     // Classes that can be injected by this Component
     fun inject(activity: MainActivity)
